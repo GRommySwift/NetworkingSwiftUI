8c9178ae6372bfcdbfc76393e370c6f76ff1e65a
GRoMMy <romaholovai@gmail.com>
2023-07-28 00:10:11 +0200

    Initial Commit

---
+++ NetworkingSwiftUI/NetworkingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingSwiftUIApp.swift
+//  NetworkingSwiftUI
+//
+//  Created by Roman on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
